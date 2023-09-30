8b4ae8d0ce8e98c029c2aaf9e3dcbcc05d172bb7
MattlDunn <mattldunn10@gmail.com>
2023-09-30 01:43:09 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Matt Dunn on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
